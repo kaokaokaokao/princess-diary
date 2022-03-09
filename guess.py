2199cf0bcc993ce8dbee9eba406ba8f022063f98
keer hu <33028352@qq.com>
2022-03-09 22:48:19 +0800

    got it!

--- guess.py
+++ guess.py
@@ -12,17 +12,19 @@ result = random_number()
 list1 = []
 for i in result:
   list1.append(i)
-print(list1)
 
 def guess(number):
   A = 0
   B = 0
-  for j in range(len(number)):
-    print(number[j],list1[j])
-    if number[j] == list1[j]:
+  length = len(number)
+  for j in range(length):
+    comp1 = int(number[j])
+    if comp1 == list1[j]:
       A += 1
-      result.remove(number[j])
-    elif int(number[j]) in result:
+      result.remove(comp1)
+  for k in range(length):
+    comp2 = int(number[k])
+    if comp2 in result:
       B += 1
   return A,B
 
