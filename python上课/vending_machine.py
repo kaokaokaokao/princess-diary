81a0a620a7896e765089566efae6ec904c1b65f2
keer hu <33028352@qq.com>
2022-04-09 22:14:59 +0800

    2022.4.9.2

--- python上课/vending_machine.py
+++ python上课/vending_machine.py
@@ -1,20 +1,24 @@
 stock = ["农夫山泉","可口可乐","水溶c100","东方树叶","尖叫"]
 actual_stock = ["农夫山泉","可口可乐","水溶c100","东方树叶","尖叫"]
 price = [2,5,6,5,4]
+total_price = 0
+print(stock)
 while True:
-  customer = input("请输入您要购买的物品的编号(1-5) 输入“清单”可以查询物品清单 输入“退出”可以结束购物 \n")
+  customer = input("请输入您要购买的物品的名称 输入“清单”可以查询物品清单 输入“退出”可以结束购物 \n")
   if customer == "清单":
     print(f"当前贩卖机售卖的物品为: {stock} \n")
-  elif customer == "结束":
-    print("谢谢光临！")
+  elif customer == "退出":
+    print(f"您的购物总金额是: {total_price}, 谢谢光临！")
     break
   else:
-    customer = int(customer) - 1
-      print(f"{stock[customer]}的价格是: {price[customer]}")
-      confirm = input("您确定要购买吗")
+    if customer in actual_stock:
+      index = stock.index(customer)
+      print(f"{stock[index]}的价格是: {price[index]}")
+      confirm = input("您确定要购买吗  ")
       if confirm == "是":
-        del actual_stock[customer]
+        total_price += price[index]
+        actual_stock.remove(customer)
       elif confirm == "否":
         pass
     else:
-      print("当前物品已售罄")
+      print("非常抱歉，该物品已售罄")
